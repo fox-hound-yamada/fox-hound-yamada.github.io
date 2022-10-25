e1d42fe0905eb08c842e7eea7bb2819232e52d00
kenta.yamada <42665244+yamada-kenta@users.noreply.github.com>
2022-10-25 16:29:37 +0900

    codeQuiz

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const endpoint = "https://script.google.com/macros/s/AKfycbxbH5AhKk3nUogybpNyQCeBv9ZbmM2a6OSRc-0qp6O1QFheB89yn2nBoOIAt23CtSuYiQ/exec";
+const endpoint = "https://hooks.slack.com/services/T055N5YFW/BUMNSA9PV/lvzJP3dkkmquYszmGmvXv2F"
 
 function openModal() {
     if (validateInput()) {
@@ -14,37 +14,58 @@ function closeModal() {
 }
 
 function validateInput() {
-    const name = $('input[name=name]').val()
-    const message = $('textarea[name=message]').val()
-    if (name == '') {
-        $('input[name=name]').addClass('is-danger')
-        $('input[name=name]').focus()
+    const title = $('input[name=title]').val()
+    const description = $('textarea[name=description]').val()
+    const language = $('input[name=language]').val()
+    const difficulty = $('input[name=difficulty]').val()
+
+    if (title == '') {
+        $('input[name=title]').addClass('is-danger')
+        $('input[name=title]').focus()
         return false
     } else {
-        $('input[name=name]').removeClass('is-danger')
+        $('input[name=title]').removeClass('is-danger')
     }
-    if (message == '') {
-        $('textarea[name=message]').addClass('is-danger')
-        $('textarea[name=message]').focus()
+    if (description == '') {
+        $('textarea[name=description]').addClass('is-danger')
+        $('textarea[name=description]').focus()
         return false
     } else {
-        $('textarea[name=message]').removeClass('is-danger')
+        $('textarea[name=description]').removeClass('is-danger')
     }
+    if (language == '') {
+        $('input[name=language]').addClass('is-danger')
+        $('input[name=language]').focus()
+        return false
+    } else {
+        $('input[name=language]').removeClass('is-danger')
+    }
+    if (difficulty == '') {
+        $('input[name=difficulty]').addClass('is-danger')
+        $('input[name=difficulty]').focus()
+        return false
+    } else {
+        $('input[name=difficulty]').removeClass('is-danger')
+    }
+
     return true
 }
 
 function inputCopy() {
-    const name = $('input[name=name]').val()
-    const answer1 = $('input[name=answer1]:checked').val();
-    const answer2 = $('input[name=answer2]:checked').val();
-    const answer3 = $('input[name=answer3]:checked').val();
-    const message = $('textarea[name=message]').val()
+    const title = $('input[name=title]').val()
+    const description = $('textarea[name=description]').val().replace(/\n/g,"<br>\n")
+    const language = $('input[name=language]').val()
+    const difficulty = $('input[name=difficulty]').val()
+    const example = $('textarea[name=example]').val().replace(/\n/g,"<br>\n")
+    const template = $('textarea[name=template]').val().replace(/\n/g,"<br>\n")
 
-    $('#name').text(name)
-    $('#answer1').text(answer1)
-    $('#answer2').text(answer2)
-    $('#answer3').text(answer3)
-    $('#message').text(message)
+    $('#title').text(title)
+    $('#description').html(description)
+    $('#language').text(language)
+    $('#difficulty').text(difficulty)
+    $('#example').html(example)
+    $('#template').html(template)
+
 }
 
 function closeAllModals() {
@@ -58,23 +79,83 @@ function post() {
     if (done == '送信完了') {
         return true
     }
-    const name = $('#name').text()
-    const answer1 = $('#answer1').text()
-    const answer2 = $('#answer2').text()
-    const answer3 = $('#answer3').text()
-    const message = $('#message').text()
+
+    const title = $('#title').text()
+    const description = $('#description').text()
+    const language = $('#language').text()
+    const difficulty = $('#difficulty').text()
+    const example = $('#example').text()
+    const template = $('#template').text()
 
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: endpoint,
-        dataType: 'jsonp',
-        data: {
-            name: name,
-            answer1: answer1,
-            answer2: answer2,
-            answer3: answer3,
-            message: message
-        },
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(
+            {
+                "blocks": [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": title,
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": description,
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "plain_text",
+                                "text": `:black_nib: Language : ${language}`,
+                                "emoji": true
+                            },
+                            {
+                                "type": "plain_text",
+                                "text": `:signal_strength: Difficulty : ${difficulty}`,
+                                "emoji": true
+                            },
+                            {
+                                "type": "plain_text",
+                                "text": ":calendar: Limit : 2022/10/31",
+                                "emoji": true
+                            }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "plain_text",
+                                "text": `:memo: Example\n ${example}`,
+                                "emoji": true
+                            }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": ":page_facing_up: Template\n" + template
+                            }
+                        ]
+                    },
+                ]
+            },
+        ),
         success: out => {
             $('.modal-card-title').text('送信完了')
             $('.modal-card-body').html(out.message + '<br><span>ブラウザを閉じて終了してください</span>')
